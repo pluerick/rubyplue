4facc19550c6e84aaf86c3ca2b17753b43b19f3f
pluerick <33692770+pluerick@users.noreply.github.com>
2023-03-19 08:32:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,25 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+ if (message.content.startsWith('?')) {
+    // Send the user's message to the OpenAI API and get a response
+    const userMessage = message.content.slice(1);
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: `Ask ChatGPT a question: ${userMessage}`,
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1,
+      stop: ['\n']
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY
+      }
+    });
+
+    // Send the response back to the user
+    const botMessage = response.data.choices[0].text.trim();
+    message.reply(botMessage);
   }
 });
 
