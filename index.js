92ee8ee864af0a96e324fe8ae80cd2dcd4e20f5b
pluerick <33692770+pluerick@users.noreply.github.com>
2023-03-19 10:16:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ client.on('message', async message => {
     });
 
     // Send the response back to the user
-    const botMessage = response.data;
+    const botMessage = response.data.choices[0].text.replace(/(\r\n|\n|\r)/gm, "");
     message.reply(botMessage);
   }
 });
