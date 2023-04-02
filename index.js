24ce9c3cf81c9ed82860c097a7fa8f8ddb7e1de1
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-02 12:22:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -51,32 +51,63 @@ client.on('message', async message => {
     }
     
     // Handle the "generate" command
-    if (command === 'generate') {
-      // Get the name of the Discord server where the command was generated
-      const serverName = message.guild.name;
-      
-      // Set up a Firebase Realtime Database reference
-      const dbRef = admin.database().ref(`test1/${serverName}`);
-      
-      // Check if the server's database table exists, and create it if it doesn't
-      dbRef.once("value", snapshot => {
-        if (!snapshot.exists()) {
-          dbRef.set({
-            message: "This is the first message for this server!"
-          });
-          
-          message.reply(`A new database table has been created for ${serverName}.`);
-        }
-        else {
-          message.reply(`The database table for ${serverName} already exists.`);
-        }
-      }, error => {
-        console.error(error);
-        message.reply(`Sorry, there was an error accessing the database.`);
-      });
-    }    
+// Handle the "generate" command
+if (command === 'generate') {
+  // Get the name of the Discord server where the command was generated
+  const serverName = message.guild.name;
 
+  // Set up a Firebase Realtime Database reference
+  const dbRef = admin.database().ref(`test1/${serverName}`);
+
+  // Check if the server's database table exists, and create it if it doesn't
+  dbRef.once("value", snapshot => {
+    if (!snapshot.exists()) {
+      // Create a new database table with 25 randomly generated rooms
+      const rooms = generateRooms();
+      dbRef.set({ rooms });
+
+      message.reply(`A new database table has been created for ${serverName}.`);
+    } else {
+      message.reply(`The database table for ${serverName} already exists.`);
+    }
+  }, error => {
+    console.error(error);
+    message.reply(`Sorry, there was an error accessing the database.`);
+  });
+}
+
+// Function to generate 25 randomized rooms
+function generateRooms() {
+  const rooms = {};
+
+  // Define the cardinal directions
+  const directions = ["north", "south", "east", "west"];
+
+  // Generate 25 rooms
+  for (let i = 1; i <= 25; i++) {
+    const id = `room-${i}`;
+    const room = {
+      name: `Room ${i}`,
+      description: `This is Room ${i}`,
+    };
+
+    // Connect the room to random neighboring rooms
+    for (const direction of directions) {
+      const neighborId = `room-${getRandomInt(1, 25)}`;
+      room[direction] = neighborId;
+    }
+
+    // Add the room to the rooms object
+    rooms[id] = room;
   }
+
+  return rooms;
+}
+
+// Function to generate a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 });
 
 client.login(process.env.TOKEN);
