e2e967a3f4e8a5739726b7592c44fe51f1d14823
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-11 09:23:25 -0500

    debug 3

--- index.js
+++ index.js
@@ -134,6 +134,7 @@ if (command === 'north') {
             // Update the player's current room to the room to the north
             const newRoomID = snapshot.val().north;
             const playerRef = playersRef.child(Object.keys(snapshot.val())[0]);
+            console.log('debug 3', playerRef);
             playerRef.update({ current_room: newRoomID }, (error) => {
               if (error) {
                 message.reply(`Sorry, ${playerName}, there was an error updating your current room.`);
