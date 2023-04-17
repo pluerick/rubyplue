7bf85c021d73b451409e8469d7cf1a93d11a8ab0
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-17 12:13:34 -0500

    sea otter build xtreme

--- index.js
+++ index.js
@@ -527,16 +527,16 @@ async function generateHaiku() {
 
 // this function generates an image with opanai then returns the URL to the image
 async function generateImage() {
-  const openai = require('openai');
-  
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
   const response = await openai.createImage({
-    prompt: 'dragon breathing fire',
-    n: 1,
+    prompt: "A cute baby sea otter",
+    n: 2,
     size: "1024x1024",
   });
-  
-  imageURL = response.data.data[0].url;
-  return imageURL;
   
 }
 async function drawMap() {
