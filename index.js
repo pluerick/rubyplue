e7f556066c6ea0237c5c569a746dffa8cdd86b75
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-17 12:59:30 -0500

    lil smoll

--- index.js
+++ index.js
@@ -410,7 +410,7 @@ if (command === 'image') {
     const response = await openai.createImage({
       prompt: 'A cute baby sea otter',
       n: 2,
-      size: '1024x1024',
+      size: '256x256',
     });
 
     if (response.url) {
