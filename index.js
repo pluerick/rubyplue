c3a46afe5bb1b2cb33d3f13e42a006a2d26cd89c
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-19 12:07:12 -0500

    g

--- index.js
+++ index.js
@@ -420,7 +420,14 @@ if (command === 'image') {
     const response = JSON.parse(stdout);
     const imgURL = response.data[0].url;
     console.log(`Generated image URL: ${imgURL}`);
-    message.reply({ files: [imgURL]});
+    const imageEmbed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Example Image')
+      .setImage('https://oaidalleapiprodscus.blob.core.windows.net/private/org-XjrCUPKR4fXAzcf0rGYObghI/user-TFJfurQEBcwjQPFzCiYSA29L/img-IRbhgmtAR0U2GzsRC1YA9KI2.png?st=2023-04-19T15%3A51%3A06Z&se=2023-04-19T17%3A51%3A06Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-19T16%3A08%3A33Z&ske=2023-04-20T16%3A08%3A33Z&sks=b&skv=2021-08-06&sig=6uz6GeArGCCQYXQFaGk1cc0d4AuoZIGvbrvCOFS9adw%3D');
+
+    msg.channel.send({ embeds: [imageEmbed] });
+    
+
   });
 }
 
