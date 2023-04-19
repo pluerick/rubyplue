3037d0f425e30ab0a28fc77ba9b7c67adecd6a24
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-19 16:25:10 -0500

    test

--- index.js
+++ index.js
@@ -588,12 +588,13 @@ async function generateRoomImage() {
     const response = JSON.parse(stdout);
     console.log('response:', response);
     const imgURL = response.data[0].url;
-
+  const roomimgURL = imgURL;
+  return roomimgURL;
   });
   
-  const roomimgURL = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-XjrCUPKR4fXAzcf0rGYObghI/user-TFJfurQEBcwjQPFzCiYSA29L/img-sCUvzjTcqw86ychQtv51pptc.png?st=2023-04-19T16%3A25%3A07Z&se=2023-04-19T18%3A25%3A07Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-19T15%3A19%3A01Z&ske=2023-04-20T15%3A19%3A01Z&sks=b&skv=2021-08-06&sig=6rlanULxgrUBA6UPaljCywp1Ib1tRIob3sZNEkzHb0w%3D';
+  //const roomimgURL = 'https://oaidalleapiprodscus.blob.core.windows.net/private/org-XjrCUPKR4fXAzcf0rGYObghI/user-TFJfurQEBcwjQPFzCiYSA29L/img-sCUvzjTcqw86ychQtv51pptc.png?st=2023-04-19T16%3A25%3A07Z&se=2023-04-19T18%3A25%3A07Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/png&skoid=6aaadede-4fb3-4698-a8f6-684d7786b067&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-04-19T15%3A19%3A01Z&ske=2023-04-20T15%3A19%3A01Z&sks=b&skv=2021-08-06&sig=6rlanULxgrUBA6UPaljCywp1Ib1tRIob3sZNEkzHb0w%3D';
   //const roomimgURL = imgURL;
-  return roomimgURL;
+  //return roomimgURL;
 }
 
 
