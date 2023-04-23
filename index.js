9ac4a39921ded835935855ba3280e3646e79c6e3
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-23 15:30:42 -0500

    maybe

--- index.js
+++ index.js
@@ -582,7 +582,7 @@ for (const direction of directions) {
 }
 exitString = exitString.slice(0, -2) + ".";
 // Add the exit string to the end of the description
-embed.description += ` ${exitString}`;
+embed.Description += ` ${exitString}`;
 
 // Return the embed
 return embed;
