a5830aac1b5fe6ec5e868e680a07f8673cb18d37
pluerick <33692770+pluerick@users.noreply.github.com>
2023-04-25 11:24:27 -0500

    test

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const request = require('request');
 let imagePrompt = "generate an image that looks photo realistic.";
 let descPrompt = 'From the second person perspective of a person as they enter a room, describe a room. Describe evidence and clues to things or creatures that may have been there previously.  Since other systems will come up with the monsters, traps, and weapons dont mention those. Dont mention actions taken by the player or changes to the room. Try not to use language that would be considered offensive when generating images later like blood';
 let worldDesc = 'a dark dank dungeon made of stone. there are torches on the walls every so often and creepy dripping sounds and small critters running around';
-global.descString =  'test';
+global.descString =  '';
 
 
 // Parse the service account key JSON string from the environment variable
@@ -690,7 +690,7 @@ playersRef.orderByChild("current_room").equalTo(currentRoomID).once("value", (sn
       console.log('othersherestring inside for loop: ', othersHereString);
     }
       othersHereString = othersHereString.slice(0, -2) + ".";
-    embed.setDescription(currentRoom.description + '\n\n' + exitString + '\n\n' + othersHereString);
+    global.descString = currentRoom.description + '\n\n' + exitString + '\n\n' + othersHereString;
     console.log('othersherestring inside if statement: ', othersHereString);
   }
 });
